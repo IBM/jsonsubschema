8f100dfca9a19b40693a94bcc6ecdb783c0b5c6b
Andrew Habib <andrew.habib@ibm.com>
2019-06-02 19:19:45 -0400

    typo + more numeric tests

--- python/test/test_numeric.py
+++ python/test/test_numeric.py
@@ -6,16 +6,12 @@ Created on May 30, 2019
 import unittest
 import warnings
 
+from jsonschema.exceptions import SchemaError
+
 from subschemachecker import Checker
 
 
 class TestIntegerSubtype(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        print("************************")
-        print("Testing integer subtypes")
-        print("************************")
 
     def test_identity(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -153,7 +149,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertTrue(Checker(s2, s1).is_subschema())
 
-    def test_mulfof1(self):
+    def test_mulOf1(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -163,7 +159,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof2(self):
+    def test_mulOf2(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -173,7 +169,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof3(self):
+    def test_mulOf3(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -183,7 +179,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof_min(self):
+    def test_mulOf_min(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -193,7 +189,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof_min_min(self):
+    def test_mulOf_min_min(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10, "minimum": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -203,7 +199,7 @@ class TestIntegerSubtype(unittest.TestCase):
         with self.subTest():
             self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof_min_min_max(self):
+    def test_mulOf_min_min_max(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer", "multipleOf": 10, "minimum": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -216,51 +212,202 @@ class TestIntegerSubtype(unittest.TestCase):
 
 class TestNumberSubtype(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        print("************************")
-        print("Testing number subtypes")
-        print("************************")
-
-    def test_identity_number(self):
+    def test_identity(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "number"}
         s2 = s1
         self.assertTrue(Checker(s1, s2).is_subschema())
 
-    def test_min_num_num(self):
+    def test_min_min(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
-              "type": "number", "minimum": 1.5}
+              "type": "number", "minimum": 5}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "number", "minimum": 1}
-        self.assertTrue(Checker(s1, s2).is_subschema())
-        self.assertFalse(Checker(s2, s1).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_max_int_int(self):
+    def test_max_max(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
-              "type": "integer", "maximum": 10}
+              "type": "number", "maximum": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
-              "type": "integer", "maximum": 5}
-        self.assertFalse(Checker(s1, s2).is_subschema())
-        self.assertTrue(Checker(s2, s1).is_subschema())
+              "type": "number", "maximum": 5}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s2, s1).is_subschema())
 
-    def test_max_int_min_int(self):
+    def test_max_min(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
-              "type": "integer", "maximum": 10}
+              "type": "number", "maximum": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
-              "type": "integer", "minimum": 5}
-        self.assertFalse(Checker(s1, s2).is_subschema())
-        self.assertFalse(Checker(s2, s1).is_subschema())
+              "type": "number", "minimum": 5}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_min_max(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 10}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "maximum": 20}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_min_max_min_max1(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 10}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 1, "maximum": 20}
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_min_max_min_max2(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 20}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 10, "maximum": 20}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s2, s1).is_subschema())
+
+    def test_min_max_min_max3(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 20}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 40, "maximum": 100}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_xmin_max_min_max(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 20}
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_xmin_max_min_xmax(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 20, "exclusiveMaximum": True}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_xmin_xmax_min_max(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20, "exclusiveMaximum": True}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 20}
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_min_max_xmin_xmax1(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20, "exclusiveMaximum": True}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 6, "maximum": 19}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s2, s1).is_subschema())
+
+    def test_min_max_xmin_xmax2(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20, "exclusiveMaximum": True}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 6, "maximum": 20}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_xmin_xmax_xmin_xmax(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": False, "maximum": 20, "exclusiveMaximum": True}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "exclusiveMinimum": True, "maximum": 20, "exclusiveMaximum": True}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s2, s1).is_subschema())
+
+    def test_mulOf1(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 10.5}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number"}
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_mulOf2(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 1.5}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 6}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertTrue(Checker(s2, s1).is_subschema())
+
+    def test_mulOf3(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": .5}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": -.5}
+        self.assertRaises(SchemaError, Checker, s1, s2)
+
+
+    def test_mulOf_min(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 10}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_mulOf_min_min(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 10, "minimum": 10}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5}
+        with self.subTest():
+            self.assertTrue(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
+
+    def test_mulOf_min_min_max(self):
+        s1 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "multipleOf": 10, "minimum": 10}
+        s2 = {"$schema": "http://json-schema.org/draft-04/schema",
+              "type": "number", "minimum": 5, "maximum": 500}
+        with self.subTest():
+            self.assertFalse(Checker(s1, s2).is_subschema())
+        with self.subTest():
+            self.assertFalse(Checker(s2, s1).is_subschema())
 
 
 class TestNumericSubtype(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        print("************************")
-        print("Testing numeric subtypes")
-        print(" integers <?--?> numbers")
-        print("************************")
 
     def test_int_num(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -278,7 +425,7 @@ class TestNumericSubtype(unittest.TestCase):
         self.assertFalse(Checker(s1, s2).is_subschema())
         self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof_num_min_int(self):
+    def test_mulOf_num_min_int(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "number", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
@@ -286,14 +433,10 @@ class TestNumericSubtype(unittest.TestCase):
         self.assertFalse(Checker(s1, s2).is_subschema())
         self.assertFalse(Checker(s2, s1).is_subschema())
 
-    def test_mulfof_num_int(self):
+    def test_mulOf_num_int(self):
         s1 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "number", "multipleOf": 10}
         s2 = {"$schema": "http://json-schema.org/draft-04/schema",
               "type": "integer"}
         self.assertTrue(Checker(s1, s2).is_subschema())
         self.assertFalse(Checker(s2, s1).is_subschema())
-
-
-if __name__ == "__main__":
-    unittest.main()
