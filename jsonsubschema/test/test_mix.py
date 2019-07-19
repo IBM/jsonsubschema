4a353b15f9454a3794bbc28140b15429450adc3e
Andrew Habib <andrew.habib@ibm.com>
2019-07-18 22:12:35 -0400

    enum tests

--- jsonsubschema/test/test_mix.py
+++ jsonsubschema/test/test_mix.py
@@ -93,12 +93,45 @@ class TestMixedTypes(unittest.TestCase):
             self.assertTrue(isSubschema(s2, s1))
 
     def test_enum1(self):
-        s1 = {"enum": [1,2,"test", False]}
-        s2 = {"type": ["integer", "string"], "minimum": 10, "enum": [1,2]}
+        s1 = {"enum": [1, 2, "test", False]}
+        s2 = {"type": ["integer", "string"], "minimum": 10, "enum": [1, 2]}
         with self.subTest():
             self.assertFalse(isSubschema(s1, s2))
         with self.subTest():
             self.assertTrue(isSubschema(s2, s1))
+
+    def test_enum2(self):
+        s1 = {"allOf": [{"enum": [1, 2, 3]}, {
+            "type": "integer"}], "enum": [3, 4, 5]}
+        s2 = {"type": "integer", "enum": [1, 2, 3]}
+        with self.subTest():
+            self.assertTrue(isSubschema(s1, s2))
+        with self.subTest():
+            self.assertFalse(isSubschema(s2, s1))
+
+    def test_enum3(self):
+        s1 = {"enum": [3, 4, 5]}
+        s2 = {"enum": [1, 2, 3]}
+        with self.subTest():
+            self.assertFalse(isSubschema(s1, s2))
+        with self.subTest():
+            self.assertFalse(isSubschema(s2, s1))
+
+    def test_enum4(self):
+        s1 = {"enum": [3, 4, 5]}
+        s2 = {"enum": [4, 5, 3]}
+        with self.subTest():
+            self.assertTrue(isSubschema(s1, s2))
+        with self.subTest():
+            self.assertTrue(isSubschema(s2, s1))
+
+    def test_enum5(self):
+        s1 = {"enum": [3, 4, 5], "allOf": [{"enum": [1,2]}]}
+        s2 = {"enum": [4, 5, 3]}
+        with self.subTest():
+            self.assertTrue(isSubschema(s1, s2))
+        with self.subTest():
+            self.assertFalse(isSubschema(s2, s1))
 
     def test_top_nottop(self):
         s1 = {}
