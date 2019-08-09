54219fd94ecf4dc9a0b163e03b1ad90460de63a9
Andrew Habib <andrew.a.habib@gmail.com>
2019-08-08 22:14:24 -0400

    Add API for join and meet operations; no tests though

--- jsonsubschema/__init__.py
+++ jsonsubschema/__init__.py
@@ -7,6 +7,8 @@ from jsonsubschema import api
 from jsonsubschema import config
 
 isSubschema = api.isSubschema
+meet = api.meet
+join = api.join
 
 set_debug = config.set_debug
 set_warn_uninhabited = config.set_warn_uninhabited
--- jsonsubschema/api.py
+++ jsonsubschema/api.py
@@ -27,9 +27,7 @@ class JSONSubSchemaFactory(json.JSONDecoder):
         return canonicalize_dict(d)
 
 
-def isSubschema(s1, s2):
-    ''' Entry point for schema subtype checking. '''
-
+def prepare_operands(s1, s2):
     # Validate both lhs and rhs schemas before starting the subtype checking.
     # Subtyping of invalid schemas is erroneous.
 
@@ -37,10 +35,30 @@ def isSubschema(s1, s2):
     print_db("LHS", s1)
     s1 = canonicalize_json(s1)
     print_db("LHS_canonical", s1)
+
     print_db()
+
     validate_schema(s2)
     print_db("RHS", s2)
     s2 = canonicalize_json(s2)
     print_db("RHS_canonical", s2)
 
+    return s1, s2
+
+
+def isSubschema(s1, s2):
+    ''' Entry point for schema subtype checking. '''
+    s1, s2 = prepare_operands(s1, s2)
     return s1.isSubtype(s2)
+
+
+def meet(s1, s2):
+    ''' Entry point for schema meet operation. '''
+    s1, s2 = prepare_operands(s1, s2)
+    return s1.meet(s2)
+
+
+def join(s1, s2):
+    ''' Entry point for schema meet operation. '''
+    s1, s2 = prepare_operands(s1, s2)
+    return s1.join(s2)
