60f0b945cf7c15cac8ea4307bfd5ec87de70d561
Andrew Habib <andrew.a.habib@gmail.com>
2021-04-23 11:35:28 +0200

    Improve cli by using argparse

--- jsonsubschema/cli.py
+++ jsonsubschema/cli.py
@@ -3,26 +3,27 @@ Created on June 24, 2019
 @author: Andrew Habib
 '''
 
-import sys
+import argparse
 
 from jsonsubschema._utils import load_json_file
 from jsonsubschema.api import isSubschema
 
 
 def main():
+    ''' CLI entry point for jsonsubschema '''
+    
+    parser = argparse.ArgumentParser(description='CLI for ssonsubschema tool which checks whether a LHS JSON schema is a subschema (<:) of another RHS JSON schema.')
+    parser.add_argument('LHS', metavar='lhs', type=str, help='Path to the JSON file which has the LHS JSON schema')
+    parser.add_argument('RHS', metavar='rhs', type=str, help='Path to the JSON file which has the RHS JSON schema')
 
-    assert len(
-        sys.argv) == 3, "jsonsubschema cli takes exactly two arguments lhs_schema and rhs_schema"
+    args = parser.parse_args()
+    s1_file_path = args.LHS
+    s2_file_path = args.RHS
 
-    s1_file = sys.argv[1]
-    s2_file = sys.argv[2]
-
-    s1 = load_json_file(s1_file, "LHS file:")
-    s2 = load_json_file(s2_file, "RHS file:")
+    s1 = load_json_file(s1_file_path, "LHS file:")
+    s2 = load_json_file(s2_file_path, "RHS file:")
 
     print("LHS <: RHS", isSubschema(s1, s2))
-    print("RHS <: LHS", isSubschema(s2, s1))
-
 
 if __name__ == "__main__":
 
