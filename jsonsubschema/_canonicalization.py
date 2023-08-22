a94dd49de3013e7fbea06675b28da3b56a483451
刘奕聪 <imyikong@gmail.com>
2023-08-22 11:18:14 +0000

    Remove numpy
    Signed-off-by: 刘奕聪 <imyikong@gmail.com>

--- jsonsubschema/_canonicalization.py
+++ jsonsubschema/_canonicalization.py
@@ -6,7 +6,7 @@ Created on June 24, 2019
 import copy
 import jsonschema
 import numbers
-import numpy
+import math
 import sys
 
 import jsonsubschema._constants as definitions
@@ -19,6 +19,7 @@ from jsonsubschema._checkers import (
 )
 from jsonsubschema.exceptions import UnsupportedEnumCanonicalization
 
+_nan = float("nan")
 TOP = {}
 BOT = {"not": {}}
 
@@ -278,8 +279,8 @@ def rewrite_enum(d):
             if utils.is_int_equiv(i):
                 ret["anyOf"].append(
                     {"type": "integer", "minimum": i, "maximum": i})
-            elif numpy.isnan(i):
-                ret["anyOf"].append({"type": "number", "enum": [numpy.NaN]})
+            elif math.isnan(i):
+                ret["anyOf"].append({"type": "number", "enum": [_nan]})
             else:
                 ret["anyOf"].append(
                     {"type": "number", "minimum": i, "maximum": i})
